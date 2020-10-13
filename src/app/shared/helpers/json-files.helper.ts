ff20d19b3829d777977a4784aa8c58c6a30a7227
riquemath <fredabisai@gmail.com>
2020-10-13 11:00:01 +0300

    register all registered headers file in json-files helper

--- src/app/shared/helpers/json-files.helper.ts
+++ src/app/shared/helpers/json-files.helper.ts
@@ -2,4 +2,5 @@ declare var require: any;
 
 export const JSON_FILES = {
   payload: require('../../../assets/json/payload.json'),
+  allRegisteredHeaders: require('../../../assets/json/all-registered-headers.json'),
 };
