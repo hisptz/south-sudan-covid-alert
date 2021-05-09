0b1ba890a5e297fb1d818ab63b411204fbfb1368
riquemath <fredabisai@gmail.com>
2021-05-09 09:34:02 +0300

    Create events service to get all events by program id

---
+++ src/app/shared/services/events.service.ts
@@ -0,0 +1,68 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { PROGRAM_ID, ORGUNIT_ID } from '../models/config.model';
+import { apiLink } from '../../../assets/configurations/apiLink';
+import { catchError } from 'rxjs/operators';
+import { from, Observable, throwError } from 'rxjs';
+import { PromiseService } from './promise.service';
+import { getDataPaginationFilters } from '../helpers/request.helper';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class EventsService {
+  constructor(
+    private httpClient: HttpClient,
+    private promiseService: PromiseService,
+  ) {}
+
+  getEventsByProgramIdObservable({
+    programId = PROGRAM_ID,
+    pageFilter = '',
+    fields,
+    pageSize = 50,
+  }): Observable<any> {
+    const queries = `program=${programId}&ou=${ORGUNIT_ID}&ouMode=DESCENDANTS&order=eventDate:DESC&${pageSize}`;
+    const url = `${apiLink}events.json?page=${pageFilter}&${queries}&${fields}`;
+    return this.httpClient
+      .get(url)
+      .pipe(catchError((error) => throwError(error)));
+  }
+
+  async getEventsByProgramIdPromise() {
+    let events = [];
+    try {
+      const fields = `fields=program,event,eventDate,orgUnit,orgUnitName,dataValues[dataElement,value]`;
+      const pagingDetails = await this.geteventsPagingDetails();
+      const pagingFilters = getDataPaginationFilters(pagingDetails, 50);
+      if (pagingFilters?.length) {
+        for (const pageFilter of pagingFilters) {
+          const eventsObservable: Observable<Function> = this.getEventsByProgramIdObservable(
+            { pageFilter, fields },
+          );
+          const eventsResult = await this.promiseService.getPromiseFromObservable(
+            eventsObservable,
+          );
+          events = eventsResult?.events?.length
+            ? [...events, ...eventsResult?.events]
+            : [...events];
+        }
+      }
+    } catch (e) {
+      throw new Error(e?.message || `Failed to fetch events`);
+    } finally {
+      return events;
+    }
+  }
+
+  getEvents() {
+    return from(this.getEventsByProgramIdPromise());
+  }
+
+  async geteventsPagingDetails() {
+    const eventsObservable: Observable<any> = this.getEventsByProgramIdObservable(
+      { fields: 'none' },
+    );
+    return await this.promiseService.getPromiseFromObservable(eventsObservable);
+  }
+}
