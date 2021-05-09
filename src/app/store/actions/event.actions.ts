afa8e8491dc44f2f1fd4bd13537018aeb2ed6d03
riquemath <fredabisai@gmail.com>
2021-05-09 17:11:24 +0300

    Resolve function names

--- src/app/store/actions/event.actions.ts
+++ src/app/store/actions/event.actions.ts
@@ -4,12 +4,12 @@ export const loadEventsByProgramId = createAction(
   '[Events] Load Events By Program ID',
 );
 
-export const loadEventsSuccessByProgramId = createAction(
+export const loadEventsByProgramIdSuccess = createAction(
   '[Events] Load Events Success By Program ID',
   props<{ data: any }>(),
 );
 
-export const loadEventsFailureByProgramId = createAction(
+export const loadEventsByProgramIdFailure = createAction(
   '[Events] Load Events Failure By Program ID',
   props<{ error: any }>(),
 );
