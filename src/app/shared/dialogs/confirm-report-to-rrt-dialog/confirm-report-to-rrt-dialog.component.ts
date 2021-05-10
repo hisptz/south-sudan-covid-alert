7258c1885bac0a3799599504efe19a3c5312dece
riquemath <fredabisai@gmail.com>
2021-05-10 13:05:30 +0300

    Create view for dialog to report to rrt

--- src/app/shared/dialogs/confirm-report-to-rrt-dialog/confirm-report-to-rrt-dialog.component.ts
+++ src/app/shared/dialogs/confirm-report-to-rrt-dialog/confirm-report-to-rrt-dialog.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-confirm-report-to-rrt-dialog',
   templateUrl: './confirm-report-to-rrt-dialog.component.html',
-  styleUrls: ['./confirm-report-to-rrt-dialog.component.css']
+  styleUrls: ['./confirm-report-to-rrt-dialog.component.css'],
 })
 export class ConfirmReportToRrtDialogComponent implements OnInit {
+  constructor(
+    public dialogRef: MatDialogRef<ConfirmReportToRrtDialogComponent>,
+    @Inject(MAT_DIALOG_DATA) public data,
+  ) {}
 
-  constructor() { }
-
-  ngOnInit(): void {
+  ngOnInit(): void {}
+  closeDialog(): void {
+    this.dialogRef.close();
   }
-
+  reportToRRT() {
+    this.dialogRef.close({ reportToRRT: true });
+  }
 }
