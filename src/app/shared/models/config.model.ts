d354c4bb31504dab3b685807b42a7a631872cfca
riquemath <fredabisai@gmail.com>
2021-05-10 10:59:38 +0300

    Add headers in config model

--- src/app/shared/models/config.model.ts
+++ src/app/shared/models/config.model.ts
@@ -9,3 +9,8 @@ export const REASON_FOR_CALLING_ID =
 export const REASON_FOR_CALLING_VALUE =
   METADATA?.dataElements?.reasonForCalling?.preferredValue || '';
 export const SYMPTOM_IDS = METADATA?.dataElements?.symptomIds;
+export const ALL_TABLE_HEADERS = JSON_FILES.allHeaders;
+export const ALL_REGISTERED_HEADERS = JSON_FILES?.allRegisteredHeaders?.headers;
+export const ALL_REGISTERED_FILTERS = JSON_FILES?.allRegisteredHeaders?.filters;
+export const REPORTED_TO_RRT_HEADERS = JSON_FILES.reportedToRRTHeaders?.headers;
+export const REPORTED_TO_RRT_FILTERS = JSON_FILES.reportedToRRTHeaders?.filters;
