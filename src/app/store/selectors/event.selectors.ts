ba7747f270589f0b811f874a4c9254540c0186ed
riquemath <fredabisai@gmail.com>
2021-05-10 11:01:31 +0300

    Get events in selector

--- src/app/store/selectors/event.selectors.ts
+++ src/app/store/selectors/event.selectors.ts
@@ -1,2 +1,25 @@
 import { createFeatureSelector, createSelector } from '@ngrx/store';
+import { AppState, getRootState } from '../reducers';
+import { map, keyBy } from 'lodash';
+export const getEventState = createSelector(
+  getRootState,
+  (state: AppState) => state.events,
+);
 
+export const getEventsByProgramId = createSelector(
+  getEventState,
+  (state) => state.events,
+);
+
+export const eventsToDisplay = createSelector(
+  getEventState,
+  getEventsByProgramId,
+  (state, events) => {
+    return map(events || [], (eventItem) => {
+      return {
+        ...keyBy(eventItem?.dataValues || [], 'dataElement'),
+        event: eventItem?.event,
+      };
+    });
+  },
+);
