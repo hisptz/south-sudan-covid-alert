fa706ff67dd583b5629d5346d729ac995acc4f48
riquemath <fredabisai@gmail.com>
2021-05-11 10:30:18 +0300

    Add case number component in shared module

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {RouterModule} from '@angular/router';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { sharedComponents } from './components';
 import { services } from './services';
 import { NgxPaginationModule } from 'ngx-pagination';
@@ -9,6 +9,8 @@ import { NgPipesModule } from 'ngx-pipes';
 import { materialModules } from './material-modules';
 import { modules } from './modules';
 import { ConfirmReportToRrtDialogComponent } from './dialogs/confirm-report-to-rrt-dialog/confirm-report-to-rrt-dialog.component';
+import { SearchPipe } from './pipes/search.pipe';
+import { CaseNumberDialogComponent } from './dialogs/case-number-dialog/case-number-dialog.component';
 
 @NgModule({
   imports: [
@@ -18,11 +20,15 @@ import { ConfirmReportToRrtDialogComponent } from './dialogs/confirm-report-to-r
     NgxPaginationModule,
     NgPipesModule,
     ...materialModules,
-    ...modules
+    ...modules,
+    FormsModule,
+    ReactiveFormsModule,
   ],
   declarations: [
     ...sharedComponents,
-    ConfirmReportToRrtDialogComponent
+    ConfirmReportToRrtDialogComponent,
+    SearchPipe,
+    CaseNumberDialogComponent
   ],
   exports: [...sharedComponents, ...materialModules, ConfirmReportToRrtDialogComponent, NgPipesModule, NgxPaginationModule, ...modules],
   providers: [...services]
