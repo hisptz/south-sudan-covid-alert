e5da766a119e605b48ca5c614a35e891ed4c3a86
riquemath <fredabisai@gmail.com>
2021-05-11 14:48:07 +0300

    Create a custom search pipe

--- src/app/shared/pipes/search.pipe.ts
+++ src/app/shared/pipes/search.pipe.ts
@@ -1,19 +1,50 @@
 import { Pipe, PipeTransform } from '@angular/core';
+import { filter, map, flattenDeep } from 'lodash';
 
 @Pipe({
-  name: 'search'
+  name: 'search',
 })
 export class SearchPipe implements PipeTransform {
+  transform(
+    items: Array<any>,
+    filters: Array<any>,
+    searchText: string,
+  ): Array<any> {
+   
+    if (!searchText) {
+      return items;
+    }
+    const formattedSearchText = searchText.toLocaleLowerCase();
 
-  transform(value: any, args?: any): any {
-
-    if(!value)return null;
-    if(!args)return value;
-
-    args = args.toLowerCase();
-
-    return value.filter(function(item){
-        return JSON.stringify(item).toLowerCase().includes(args);
+    const filteredItems = filter(items || [], (item) => {
+      const valuesArray = flattenDeep(
+        map(filters || [], (filter) => {
+          return item[filter]?.value && item[filter]?.value?.toLocaleLowerCase()
+            ? item[filter]?.value?.toLocaleLowerCase()
+            : [];
+        }),
+      );
+      const filteredValueArr = filter(valuesArray || [], (valueItem) => {
+        if (valueItem?.indexOf(formattedSearchText) >= 0) {
+          return valueItem;
+        }
+      });
+      console.log({valuesArray, filteredValueArr})
+    
+      if (filteredValueArr?.length) {
+        return item;
+      }
     });
-}
+   
+
+    return filteredItems;
+
+    // return items.filter((item) => {
+    //   const notMatchingField = Object.keys(filter).find(
+    //     (key) => item[key] !== filter[key],
+    //   );
+
+    //   return !notMatchingField; // true if matches all fields
+    // });
+  }
 }
