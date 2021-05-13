5476d3f941bcb5346c0853b92bb0cab988264250
riquemath <fredabisai@gmail.com>
2021-05-13 15:19:53 +0300

    Show notification action

--- src/app/store/actions/page-state.actions.ts
+++ src/app/store/actions/page-state.actions.ts
@@ -1,6 +1,7 @@
 import { createAction, props } from '@ngrx/store';
 import { Update } from '@ngrx/entity';
 import { PageState, NotificationState } from '../models';
+import { NotificationType } from 'src/app/shared/models/config.model';
 
 export const loadNotification = createAction(
   '[PageState/API] Load Notification',
@@ -9,7 +10,7 @@ export const loadNotification = createAction(
 
 export const showNotification = createAction(
   '[PageState/API] Show notification',
-  props<{ payload: boolean }>(),
+  props<{ message: string; notificationType?: NotificationType }>()
 );
 
 export const updateNotification = createAction(
